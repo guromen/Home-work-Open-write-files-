93bc4c62a4976e17b9339e1141265966db1d8fe4
Roman Guguzin <r-0men@yandex.ru>
2024-06-11 21:08:12 +0400

    Задание 2

--- cook_book.py
+++ cook_book.py
@@ -16,3 +16,22 @@ with open('recipes.txt', encoding = 'utf-8') as f:
         cook_book[name_dish] = ingrigients        
         f.readline()
 print(cook_book)
+
+# print(cook_book['Омлет'][0]['ingredient_name'])
+# print(cook_book['Омлет'][1]['ingredient_name'])
+# print(cook_book['Омлет'][2]['ingredient_name'])
+
+def get_shop_list_by_dishes(dishes, person_count):
+    total = {}
+    for dish in dishes:
+        if dish in cook_book:
+         for exist in cook_book[dish]:
+            if exist['ingredient_name'] not in total:
+                total[exist['ingredient_name']] = {'measure': exist['measure'], 'quantity': int(exist['quantity'])*person_count}
+            else:
+                total[exist['ingredient_name']]['quantity'] += int(exist['quantity'])*person_count
+        else:
+            print(f'Блюда "{dish}" нет в книге рецептов')
+    return total
+print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
+
