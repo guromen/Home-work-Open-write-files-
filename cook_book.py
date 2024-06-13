3004c251a6e60e8c58a95d1e33e8dadc4af34c9d
Roman Guguzin <r-0men@yandex.ru>
2024-06-13 21:25:46 +0400

    Задание 3

--- cook_book.py
+++ cook_book.py
@@ -15,6 +15,7 @@ with open('recipes.txt', encoding = 'utf-8') as f:
             ingrigients.append(ingredient_dict)
         cook_book[name_dish] = ingrigients        
         f.readline()
+
 print(cook_book)
 
 # print(cook_book['Омлет'][0]['ingredient_name'])
@@ -26,12 +27,47 @@ def get_shop_list_by_dishes(dishes, person_count):
     for dish in dishes:
         if dish in cook_book:
          for exist in cook_book[dish]:
+            #Проверка на уже существующий ингридеент 
             if exist['ingredient_name'] not in total:
-                total[exist['ingredient_name']] = {'measure': exist['measure'], 'quantity': int(exist['quantity'])*person_count}
+                total[exist['ingredient_name']] = {'measure': exist['measure'], 'quantity': exist['quantity']*person_count}
             else:
-                total[exist['ingredient_name']]['quantity'] += int(exist['quantity'])*person_count
+                #Пересчет количества
+                total[exist['ingredient_name']]['quantity'] += exist['quantity']*person_count
         else:
             print(f'Блюда "{dish}" нет в книге рецептов')
     return total
+
 print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
 
+# прочитать все три файла .txt, которые даны в исходных данных,
+# В каждом файле пересчитать количество строк,
+# Создать словарь, где ключам будет количество строк, а значением само содерждание файла с добавленными впереди 2 строчками (названием файла и количеством строк)
+# Отсортировать словарь по ключам
+# Записать в новый результирующий файл последовательно данные всех значений отсортированного словаря
+
+
+def write_file(path1, path2, path3):
+    path1 = '1.txt'
+    path2 = '2.txt'
+    path3 = '3.txt'
+
+    my_dict = {}
+    
+    with open(path1, encoding='utf-8') as f1:
+        file1 = f1.readlines()
+        my_dict[(len(file1))] = (path1) + '\n' + (str(len(file1))) + '\n' + (''.join(file1)) + '\n'
+    with open(path2, encoding='utf-8') as f2:
+        file2 = f2.readlines()
+        my_dict[(len(file2))] = (path2) + '\n' + (str(len(file2))) + '\n' + (''.join(file2)) + '\n'
+    with open(path3, encoding='utf-8') as f3:
+        file3 = f3.readlines()
+        my_dict[(len(file3))] = (path3) + '\n' + (str(len(file3))) + '\n' + (''.join(file3))
+    my_dict = dict(sorted(my_dict.items(), key=lambda item: item[0]))
+    # print(my_dict)
+
+    with open('write_file.txt', 'w', encoding='utf-8') as f:
+        for _, v in my_dict.items():
+            f.write(str(v))
+    return
+
+write_file('1.txt', '2.txt', '3.txt')
